8aa50993ac086c5c49ccd43bba8cbb30b7a88eec
mayaoting <2510147466@qq.com>
2021-02-08 23:20:55 +0800

    init project

---
+++ src/components/column-list.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar" :alt="column.title" />
+      <h5>{{ column.title }}</h5>
+      <p>{{ column.description }}</p>
+      <a href="#">进入小记</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'column-list',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+});
+</script>
+
+<style>
+</style>
