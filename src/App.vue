23cd68f25223f75deafc377732e0871feccf54c6
mayaoting <2510147466@qq.com>
2021-02-15 23:06:13 +0800

    use computed in vue3

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import columnList from './components/column-list.vue'
+import { defineComponent } from 'vue';
+import columnList from './components/column-list.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default defineComponent({
   name: 'App',
@@ -22,15 +23,27 @@ export default defineComponent({
       {
         id: 2,
         title: 'test2的专栏',
+        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
+        // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+      },
+      {
+        id: 3,
+        title: 'test3的专栏',
+        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+      },
+      {
+        id: 4,
+        title: 'test4的专栏',
         description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
         avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
       }
-    ]
+    ];
     return {
       list
-    }
+    };
   }
-})
+});
 </script>
 
 <style>
--- src/components/column-list.vue
+++ src/components/column-list.vue
@@ -1,16 +1,20 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title" />
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="#">进入小记</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" :alt="column.title" class="immm rounded-circle border border-light my-3" />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入小记</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 export interface ColumnProps {
   id: number;
   title: string;
@@ -24,9 +28,25 @@ export default defineComponent({
       type: Array as PropType<ColumnProps[]>,
       required: true
     }
+  },
+  setup(props) {
+    // 使用计算属性设置默认的avatar 图片
+    const columnList = computed(() => {
+      return props.list.map((column) => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/column.jpg');
+        }
+        return column;
+      });
+    });
+    return { columnList };
   }
 });
 </script>
 
-<style>
+<style scoped>
+.card-body img {
+  width: 52px;
+  height: 52px;
+}
 </style>
